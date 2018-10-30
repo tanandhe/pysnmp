2c794d05ae4a6676b1029d73c150dfff909928f2
Tanxinyuan <18604892102@qq.com>
2018-10-31 00:44:49 +0800

    用pysnmp实现了snmpwalk功能

---
+++ test.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 30 23:02:20 2018
+
+@author: Administrator
+"""
+
+from pysnmp.hlapi import *
+OID = '1.3.6.1.2.1.17.4.3.1.1'
+g=nextCmd(SnmpEngine(),
+           CommunityData('#DZ1SW1K!'),
+           UdpTransportTarget(('61.138.72.2', 161)),
+           ContextData(),
+           ObjectType(ObjectIdentity(OID)))
+flag = True
+while flag:
+    errorIndication,errorStatus,errorIndex,varBinds =next(g)
+    if errorIndication:
+        print(errorIndication)
+        break
+    elif errorStatus:
+        break
+        print('%s at %s' % (errorStatus.prettyPrint(),
+                            errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+    elif errorIndex:
+        break
+    else:
+        for varBind in varBinds:
+            if OID not in str(varBind[0]):
+                flag = False
+                break
+            print(' = '.join([x.prettyPrint()cc for x in varBind]))
+
