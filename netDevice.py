9f0902d45dfc1fda54edbd9f24853bdb52350024
Tanxinyuan <625812437@qq.com>
2018-11-01 21:11:42 +0800

    netDevice类添加insertIf方法，未测试

--- netDevice.py
+++ netDevice.py
@@ -5,9 +5,14 @@ Created on Thu Nov  1 17:52:07 2018
 @author: Administrator
 """
 from func import *
+import time
 
 class netDevice(object):
     def __init__(self,ip,community,nameoid="1.3.6.1.2.1.1.5.0"):
+        '''
+        初始化设备类，获取设备名称，如果获取成功status为online，反之offline
+        （网络不可达、团体字错误）
+        '''
         self.ip = ip
         self.community = community
         self.name = snmpGet(self.ip,self.community,nameoid)[0][1]
@@ -15,6 +20,43 @@ class netDevice(object):
             self.status = 'online'
         else:
             self.status = 'offline'
+
+    def insertIf(self，conn，interface,oid='1.3.6.1.2.1.31.1.1.1.1'):
+        '''
+        获取设备端口名称和索引，入库。con:数据库打开对象
+        '''
+        if self.status != 'online':
+            break
+        currentTime = time.time()
+        cur = conn.cursor()
+
+        for d in interface:
+            #先根据设备IP地址和端口索引，判断数据库中是否存在索引            
+            sql = "select id,ifName from interface where deviceIp='%s' and ifIndex='%s'" % (self.ip,d[0])
+            try:
+                res = cur.execute(sql)
+                id =  cur.fetchall()
+            except:
+                print ("查询端口失败")
+            if res:
+                #如果存在，更新端口名称和时间信息
+                sql = "UPDATE interface SET ifName='%s' time='%f' WHERE id = '%d'" % (d[1],currentTime,id)
+                try:
+                    cur.execute(sql)
+                    conn.commit()
+                except:
+                    conn.rollback()
+                    print ("更新端口失败")
+            else:
+                sql = "insert into interface(deviceIp,ifIndex,ifName,time) values ('%s','%s','%s',%f)" %\
+                 (self.ip,d[0].replace(oid+".",''),d[1],currentTime)
+                try:
+                    cur.execute(sql)
+                    conn.commit()
+                except :
+                    conn.rollback()
+                    print ("插入端口失败")
+        cur.close()  
         
 if __name__ == '__main__':
     ip="61.138.72.2"
