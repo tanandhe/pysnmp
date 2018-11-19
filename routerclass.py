5d8796a091953e6c7293074f13df1ed986d825b5
Tanxinyuan <625812437@qq.com>
2018-11-19 16:15:45 +0800

    添加了路由类文件

---
+++ routerclass.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 17:22:10 2018
+
+@author: Administrator
+"""
+
+from netDevice import *
+
+class hwSR(netDevice):
+    def arpInsert(self,oid='1.3.6.1.4.1.2011.5.25.123.1.17.1.11'):
+        if self.status != 'online':
+            return
+        arp = snmpWalk(self.ip,self.community,oid)
+        print(arp)
+        currentTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        return arp
+    
+if __name__ == '__main__':
+    conn = pymysql.connect(
+            host='127.0.0.1',
+            port=3306,
+            user='root',
+            passwd=' ',
+            db='test',
+            charset='utf8'
+            ) 
+    ip="110.19.8.13"
+    community="IP&zhwg%"
+    myDevice = hwSR(conn,ip,community,nameoid='1.3.6.1.2.1.1.5.0')
+    print (myDevice.name)   
+    arp = myDevice.arpInsert()
